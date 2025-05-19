01d89a77e3c54de2b97d4b2cd614c632fe87c12a
marmaryia <marozavamaryia@gmail.com>
2025-05-19 17:54:23 +0100

    fill in personal details

--- src/components/ContactDetails.jsx
+++ src/components/ContactDetails.jsx
@@ -1,7 +1,27 @@
+import "../styles/ContactDetails.css";
+
 function ContactDetails() {
   return (
     <section>
       <h2>Contact Details</h2>
+      <ul className="contacts-container">
+        <li className="email-container">
+          <p>marozavamaryia@gmail.com</p>
+          <button
+            onClick={() => {
+              navigator.clipboard.writeText("marozavamaryia@gmail.com");
+            }}
+          >
+            Copy
+          </button>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/maryia-m-7a26b022a/">LinkedIn</a>
+        </li>
+        <li>
+          <a href="https://github.com/marmaryia">GitHub</a>
+        </li>
+      </ul>
     </section>
   );
 }
