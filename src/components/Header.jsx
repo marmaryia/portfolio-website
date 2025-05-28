d7bf95455213987618c6aea64b0d20da6ee5fdf8
marmaryia <marozavamaryia@gmail.com>
2025-05-28 14:43:17 +0100

    make header disappear and reappear on scroll on mobile view

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,25 @@
 import "../styles/Header.css";
+import { useState, useEffect } from "react";
 
 function Header({ visibleSection }) {
+  const [position, setPosition] = useState(window.pageYOffset);
+  const [visible, setVisible] = useState(true);
+
+  function handleScroll() {
+    let moving = window.pageYOffset;
+    setVisible(position > moving);
+    setPosition(moving);
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
   return (
-    <header className="header-shadow">
+    <header className={`header-shadow ${visible ? "visible" : "hidden"} `}>
       <ul className="header-list" id="header">
         <li className={visibleSection === "about" ? "active" : ""}>
           <a href="#about">About</a>
